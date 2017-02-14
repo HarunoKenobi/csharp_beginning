983f5f2b64e1a71dd09d2b64b272e862b24acade
Haruno Kenobi <haruno.kenobi@gmail.com>
2017-02-13 22:39:48 -0200

    first commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,7 @@
+﻿class HelloWord{
+    public static void Main(){
+        System.Console.WriteLine("Digite o seu nome:");
+        var nome = System.Console.ReadLine();
+        System.Console.WriteLine($"Seja muito bem vindo ao csharp {nome}, espero que goste da experiência!");
+    }
+}
